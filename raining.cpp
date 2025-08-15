cf48e780f0bf7ee4175883a8ff28eaf70d830a01
pranavrajesh23 <pranav.rajesh@psiog.com>
2025-08-15 20:50:00 +0530

    finished rainingCheck

--- raining.cpp
+++ raining.cpp
@@ -2,6 +2,21 @@
 #include<string>
 using namespace std;
 
+void rainingCheck()
+{
+    string rain;
+    cout<<"\nIs it raining outside?(yes/no)\n";
+    cin>>rain;
+    if(rain=="yes")
+    {
+        umbrellaCheck();
+    }
+    else
+    {
+        cout<<"\nDon't bring the umbrella\n";
+    }
+}
+
 int main()
 {
     cout<<"Raining check program";
